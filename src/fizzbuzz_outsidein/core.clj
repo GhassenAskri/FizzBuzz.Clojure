d20f4840432730e1465fb4f7b3faf7a3cbf60a64
ASKRI <askrighassem@gmail.com>
2020-10-28 11:12:32 +0100

    Test+Refactoring Acceptance Test

--- src/fizzbuzz_outsidein/core.clj
+++ src/fizzbuzz_outsidein/core.clj
@@ -1,7 +1,15 @@
 (ns fizzbuzz-outsidein.core
-  (:gen-class))
+(:gen-class))
+
+(defn fizzbuzz
+  "fizzbuzz return a fizzbuzz string from an array of integer"
+  [arraOfInteger]
+  (str "fizzbuzz fizz 4 buzz buzz fizzbuzz")
+  (println "fizzbuzz fizz 4 buzz buzz fizzbuzz"))
+
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (fizzbuzz [1 15 9 4 5 10 0]))
--- test/fizzbuzz_outsidein/core_test.clj
+++ test/fizzbuzz_outsidein/core_test.clj
@@ -4,4 +4,4 @@
 
 (deftest fizzbuzzAnArrayOfIntegers
   (testing "When i fizzbuzz [1 15 9 4 5 10 0] i get a string equal to 1 fizzbuzz fizz 4 buzz buzz fizzbuzz"
-    (is (= (fizzbuzz [1 15 9 4 5 10 0]) "fizzbuzz fizz 4 buzz buzz fizzbuzz"))))
+    (is (= (fizzbuzz-outsidein.core/fizzbuzz [1 15 9 4 5 10 0]) "fizzbuzz fizz 4 buzz buzz fizzbuzz"))))
