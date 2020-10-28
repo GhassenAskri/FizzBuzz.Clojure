a344dc8b6be3e2c1f35f4fca9e8e7b901aec59e1
ASKRI <askrighassem@gmail.com>
2020-10-28 11:21:56 +0100

    test: apply fizzbuzz an one number not mul of 3 or 5

--- test/fizzbuzz_outsidein/core_test.clj
+++ test/fizzbuzz_outsidein/core_test.clj
@@ -5,3 +5,7 @@
 (deftest fizzbuzzAnArrayOfIntegers
   (testing "When i fizzbuzz [1 15 9 4 5 10 0] i get a string equal to 1 fizzbuzz fizz 4 buzz buzz fizzbuzz"
     (is (= (fizzbuzz-outsidein.core/fizzbuzz [1 15 9 4 5 10 0]) "fizzbuzz fizz 4 buzz buzz fizzbuzz"))))
+
+(deftest fizzbuzzAnumberThatIsNotMultipleOfThreeOrFive
+  (testing "When i fizzbuzz 1 i get 1 as string "
+    (is (= (fizzBuzzOneNumbre 1)))))
